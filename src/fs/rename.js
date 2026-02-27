988f7d3374a16e7acb624ed6e0da7b0ff29e807a
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-27 10:34:05 +0100

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const oldFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const newFilePath = path.join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-  // Write your code here
+  if (fs.existsSync(newFilePath)) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    fs.renameSync(oldFilePath, newFilePath);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
