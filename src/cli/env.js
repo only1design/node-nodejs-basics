185f15ad17c44fd1661195360bc98e23441ce3cd
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-27 12:58:15 +0100

    feat: add cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-  // Write your code here
+  const variables = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith('RSS_')) {
+      acc.push(`${key}: ${value}`);
+    }
+
+    return acc;
+  }, [])
+
+  console.log(variables.join("; "))
 };
 
 parseEnv();
