cd8993c66202cfb78bfb7466e52d76ead4a7fcb9
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-27 21:24:31 +0100

    feat: implement streams module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import stream from "node:stream";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-  // Write your code here
+  await stream.promises.pipeline(
+    fs.createReadStream(filePath),
+    process.stdout,
+  )
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import stream from "node:stream";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-  // Write your code here
+  await stream.promises.pipeline(
+    process.stdin,
+    fs.createWriteStream(filePath),
+  )
 };
 
 await write();
