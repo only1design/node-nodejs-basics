ba160c03536969af65eb7fd2dc740aea469d2e38
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-27 20:43:14 +0100

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+import crypto from "node:crypto";
+import stream from "node:stream";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-  // Write your code here
+  const hash = crypto.createHash('sha256');
+
+  const writable = new stream.Writable({
+    write(chunk, encoding, callback) {
+      hash.update(chunk);
+      callback();
+    }
+  });
+
+  await stream.promises.pipeline(
+    fs.createReadStream(filePath),
+    writable
+  );
+
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
