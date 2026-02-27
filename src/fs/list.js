d73b34ace04e525a3128918198d3e4fbdee0dc73
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-27 10:47:27 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const folderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-  // Write your code here
+  try {
+    console.log(fs.readdirSync(folderPath))
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
