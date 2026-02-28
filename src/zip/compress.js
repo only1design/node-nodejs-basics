573fc82607edac033f72763c6e6977347bbd55da
Vladimir Bekarev <v.bekarev@syberry.com>
2026-02-28 14:12:01 +0100

    feat: implement zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+import stream from "node:stream";
+import * as zlib from "node:zlib";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const srcPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const destPath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-  // Write your code here
+  const gzip = zlib.createGzip();
+
+  await stream.promises.pipeline(
+    fs.createReadStream(srcPath),
+    gzip,
+    fs.createWriteStream(destPath),
+  )
 };
 
 await compress();
