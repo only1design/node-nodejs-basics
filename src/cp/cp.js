1bdf12ed05156d07abecc11a13f5a3ddc0a01ad6
Vladimir Bekarev <v.bekarev@syberry.com>
2026-03-01 12:23:38 +0100

    feat: implement cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'node:child_process';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  fork(filePath, args)
 };
 
 // Put your arguments in function call to test this functionality
 spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess([1, 2, 3, 4]);
+
